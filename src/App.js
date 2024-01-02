ee2469627ca4443bd6ef1533cd111d2d6207cb34
saiyskr <saikiranreddy.yeruva@gmail.com>
2024-01-02 18:26:11 -0500

    added styles

--- src/App.js
+++ src/App.js
@@ -123,17 +123,17 @@ function App() {
       <div className='full-screen-text section-4' id = "contact">
       <h4>Contact Me</h4>
 
-          <strong><i className="fas fa-envelope"></i> :</strong>
-          <a href="mailto:syeruva2@buffalo.edu">syeruva2@buffalo.edu</a>
+          <strong><i className="fas fa-envelope bigger-icon"></i> :</strong>
+          <a href="mailto:syeruva2@buffalo.edu"><span>syeruva2@buffalo.edu</span></a>
         
-          <strong><i className="fas fa-phone"></i> :</strong>
-          <a href="tel:+17169397219">+1 (716) 939-7219</a>
+          <strong><i className="fas fa-phone bigger-icon"></i> :</strong>
+          <a href="tel:+17169397219"><span>+1 (716) 939-7219</span></a>
         
-          <strong><i className="fab fa-linkedin"></i> :</strong>
-          <a href="https://www.linkedin.com/in/saikiranreddyy/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a>
+          <strong><i className="fab fa-linkedin bigger-icon"></i> :</strong>
+          <a href="https://www.linkedin.com/in/saikiranreddyy/" target="_blank" rel="noopener noreferrer"><span>LinkedIn Profile</span></a>
        
-          <strong><i className="fab fa-github"></i> :</strong>
-          <a href="https://github.com/saiyskr/" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
+          <strong><i className="fab fa-github bigger-icon"></i> :</strong>
+          <a href="https://github.com/saiyskr/" target="_blank" rel="noopener noreferrer"><span>GitHub Profile</span></a>
         
           <strong>Address:</strong>
           Buffalo, New York , USA
