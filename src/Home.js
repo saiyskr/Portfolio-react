4fa7e9f3088b895b884156821b1818daccd9c063
saiyskr <saikiranreddy.yeruva@gmail.com>
2024-07-12 14:00:28 -0400

    added view resume

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,23 @@
-// function Home(){
-//     return(
-//         <section className='full-screen-text2 description home-section' id="home">
-      
-//       <h3>Hi , I am Sai Kiran Reddy, a <strong>full-stack software developer</strong> </h3>
-      
-//       <h3>focused on creating responsive and user-friendly <strong>web applications</strong> </h3>
-      
-//       <h3>while writing clean code.</h3>
-//       </section>
-//     )
-// }
-// export default Home;
-function Home(){
-  return(
-      <section className='home-section' id="home">
-          <div className="content">
-              <h3>Hi, I am <span>Sai Kiran Reddy</span>, a <strong>full-stack software developer</strong></h3>
-              <h3>focused on creating responsive and user-friendly <strong>web applications</strong></h3>
-              <h3>while writing clean code.</h3>
-          </div>
-      </section>
-  )
-}
+import React from 'react';
+
+const Home = () => {
+  const handleViewResume = () => {
+    window.open("https://drive.google.com/file/d/1dxGA56KFQBTMbJtow8Cr276AGRI8I07L/view?usp=sharing", '_blank');
+  };
+
+  return (
+    <section className='home-section' id="home">
+      <div className="content">
+        <h3>Hi, I am <span>Sai Kiran Reddy</span>, a <strong>full-stack software developer</strong></h3>
+        <h3>focused on creating responsive and user-friendly <strong>web applications</strong></h3>
+        <h3>while writing clean code.</h3>
+      </div>
+      <br />
+      <button className="btn btn-danger resume-btn" onClick={handleViewResume}>
+        View Resume
+      </button>
+    </section>
+  );
+};
+
 export default Home;
