63c66b0945e9d15742e71483a3e12fb3ccee92e6
saiyskr <saikiranreddy.yeruva@gmail.com>
2024-07-24 12:20:43 -0400

    type writer

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Home = () => {
   const handleViewResume = () => {
@@ -8,12 +9,38 @@ const Home = () => {
   return (
     <section className='home-section' id="home">
       <div className="content">
-        <h3>Hi, I am <span>Sai Kiran Reddy</span>, a <strong>full-stack software developer</strong></h3>
-        <h3>focused on creating responsive and user-friendly <strong>web applications</strong></h3>
+        <h3>
+          Hi, I am <span>Sai Kiran Reddy</span>, a{' '}
+          <strong>
+            <Typewriter
+              words={['full-stack software developer', 'enthusiastic coder', 'tech enthusiast']}
+              loop={0}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </strong>
+        </h3>
+        <h3>
+          focused on creating responsive and user-friendly{' '}
+          <strong>
+            <Typewriter
+              words={['web applications','software solutions']}
+              loop={0}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </strong>
+        </h3>
         <h3>while writing clean code.</h3>
       </div>
       <br />
-      <button className="btn btn-danger resume-btn m-5" onClick={handleViewResume}>
+      <button className="btn btn-danger m-5" onClick={handleViewResume}>
         View Resume
       </button>
     </section>
